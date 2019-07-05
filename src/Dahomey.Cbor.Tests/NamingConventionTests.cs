0a3c86670d79c8990673d86e84a139a0b4e03654
Michael Catanzariti <catanzariti@yahoo.com>
2019-07-05 22:57:44 +0200

    naming conventions
    * Added Snake Case naming convention
    * Add Unit test for naming conventions

---
+++ src/Dahomey.Cbor.Tests/NamingConventionTests.cs
@@ -0,0 +1,38 @@
+﻿using Dahomey.Cbor.Serialization.Conventions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dahomey.Cbor.Tests
+{
+    [TestClass]
+    public class NamingConventionTests
+    {
+        [DataTestMethod]
+        [DataRow("FooBar", "fooBar")]
+        [DataRow("fooBar", "fooBar")]
+        [DataRow("Foo", "foo")]
+        [DataRow("foo", "foo")]
+        [DataRow("", "")]
+        public void CamelCase(string srcName, string expectedName)
+        {
+            ReadOnlyMemory<byte> actualBytes = new CamelCaseNamingConvention().GetPropertyName(srcName);
+            Assert.AreEqual(expectedName, Encoding.ASCII.GetString(actualBytes.Span));
+        }
+
+        [DataTestMethod]
+        [DataRow("FooBar", "foo_bar")]
+        [DataRow("fooBar", "foo_bar")]
+        [DataRow("FooBAR", "foo_bar")]
+        [DataRow("FooBARFoo", "foo_bar_foo")]
+        [DataRow("Foo", "foo")]
+        [DataRow("foo", "foo")]
+        [DataRow("", "")]
+        public void SnakeCase(string srcName, string expectedName)
+        {
+            ReadOnlyMemory<byte> actualBytes = new SnakeCaseNamingConvention().GetPropertyName(srcName);
+            Assert.AreEqual(expectedName, Encoding.ASCII.GetString(actualBytes.Span));
+        }
+    }
+}
---
+++ src/Dahomey.Cbor/Serialization/Conventions/SnakeCaseNamingConvention.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Dahomey.Cbor.Serialization.Conventions
+{
+    public class SnakeCaseNamingConvention : INamingConvention
+    {
+        public unsafe ReadOnlyMemory<byte> GetPropertyName(string name)
+        {
+            byte[] buffer = new byte[name.Length * 2];
+            int dstIndex = 0;
+            int srcLength = name.Length;
+            bool lastIsLower = false;
+
+            for (int srcIndex = 0; srcIndex < srcLength; srcIndex++)
+            {
+                char c = name[srcIndex];
+
+                if (char.IsUpper(c))
+                {
+                    if (lastIsLower || srcIndex > 0 && srcIndex < srcLength - 1 && char.IsLower(name[srcIndex + 1]))
+                    {
+                        buffer[dstIndex++] = (byte)'_';
+                        lastIsLower = false;
+                    }
+
+                    buffer[dstIndex++] = (byte)char.ToLowerInvariant(c);
+                }
+                else
+                {
+                    lastIsLower = true;
+                    buffer[dstIndex++] = (byte)c;
+                }
+            }
+
+            return new Memory<byte>(buffer, 0, dstIndex);
+        }
+    }
+}
