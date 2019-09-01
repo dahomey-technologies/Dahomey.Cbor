32906aee0f7757a776c9f08b62e4640df89d427e
Michael Catanzariti <catanzariti@yahoo.com>
2019-09-01 21:56:09 +0200

    Sample for [question] how to specify a custom mapping between constructor parameters and class fields/properties #18

---
+++ src/Dahomey.Cbor.Tests/Issues/Issue0018.cs
@@ -0,0 +1,36 @@
+﻿using Dahomey.Cbor.Attributes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dahomey.Cbor.Tests.Issues
+{
+    [TestClass]
+    public class Issue0018
+    {
+        private class Tree
+        {
+            [CborProperty("NOTr")]
+            private readonly byte[] _r;
+
+            public byte[] R => _r;
+
+            [CborConstructor("NOTr")]
+            public Tree(byte[] r)
+            {
+                _r = r;
+            }
+        }
+        [TestMethod]
+        public void Test()
+        {
+            const string hexBuffer = "A1644E4F5472820102"; // {"NOTr": [1, 2]}
+
+            Tree tree = Helper.Read<Tree>(hexBuffer); 
+
+            Assert.IsNotNull(tree);
+            Assert.IsNotNull(tree.R);
+            Assert.AreEqual(2, tree.R.Length);
+            Assert.AreEqual(1, tree.R[0]);
+            Assert.AreEqual(2, tree.R[1]);
+        }
+    }
+}
