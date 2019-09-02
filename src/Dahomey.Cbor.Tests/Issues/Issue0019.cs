485fa19d318eb52a3006a5648394b8ea0436efb3
Michaël Catanzariti <michael.catanzariti@ubisoft.com>
2019-09-02 14:03:43 +0200

    Sample for: [question] how to allow calling the base constructor on deserialization? #19

---
+++ src/Dahomey.Cbor.Tests/Issues/Issue0019.cs
@@ -0,0 +1,51 @@
+﻿using Dahomey.Cbor.Attributes;
+using Xunit;
+
+namespace Dahomey.Cbor.Tests.Issues
+{
+    public class Issue0019
+    {
+        public class Tree : Plant
+        {
+            [CborProperty("age")]
+            public int Age { get; }
+
+            [CborConstructor]
+            public Tree(int age, string name) : base(name)
+            {
+                Age = age;
+            }
+        }
+
+        public abstract class Plant
+        {
+            [CborProperty("name")]
+            public string Name { get; }
+
+            public Plant(string name)
+            {
+                Name = name;
+            }
+        }
+
+        [Fact]
+        public void TestWrite()
+        {
+            Tree tree = new Tree(12, "foo");
+
+            const string hexBuffer = "A2636167650C646E616D6563666F6F"; // { "name": "foo", "age": 12 }
+            Helper.TestWrite(tree, hexBuffer);
+        }
+
+        [Fact]
+        public void TestRead()
+        {
+            const string hexBuffer = "A2636167650C646E616D6563666F6F"; // { "name": "foo", "age": 12 }
+            Tree tree = Helper.Read<Tree>(hexBuffer);
+
+            Assert.NotNull(tree);
+            Assert.Equal("foo", tree.Name);
+            Assert.Equal(12, tree.Age);
+        }
+    }
+}
