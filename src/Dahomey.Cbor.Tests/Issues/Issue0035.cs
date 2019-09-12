bfa646cd9b4ec171fef9839155d533373647de99
Jørgen Teunis <jorgen@solvebright.com>
2019-09-12 08:57:23 +0200

    Fix issue 35 parameter less constructor

---
+++ src/Dahomey.Cbor.Tests/Issues/Issue0035.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace Dahomey.Cbor.Tests.Issues
+{
+    public class Issue0033
+    {
+        public class Result<T>
+        {
+            public T Data { get; set; }
+            //public Result()
+            //{
+            //    // This is required now. Should not be
+            //}
+
+            public Result(T data)
+            {
+                Data = data;
+            }
+        }
+
+
+        [Fact]
+        public void TestNonRequiredEmptyConstructor()
+        {
+            var result = new Result<string>("test");
+            string hexEncoded = null;
+            var ex = Record.Exception(() =>
+            {
+                hexEncoded = Helper.Write(result);
+            });
+            Assert.Null(ex);
+            Assert.NotNull(hexEncoded);
+            Assert.Equal("A164446174616474657374", hexEncoded);
+        }
+    }
+}
