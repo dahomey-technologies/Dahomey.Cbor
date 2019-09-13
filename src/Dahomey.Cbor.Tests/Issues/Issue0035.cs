da22b56a3fd74fc9bff8a52675ec5c5c0cbace17
Michael Catanzariti <catanzariti@yahoo.com>
2019-09-14 00:21:06 +0200

    Added test for Bug: required parameterless constructor #35

---
+++ src/Dahomey.Cbor.Tests/Issues/Issue0035.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace Dahomey.Cbor.Tests.Issues
+{
+    public class Issue0035
+    {
+        public class Result<T>
+        {
+            public T Data { get; set; }
+
+            public Result(T data)
+            {
+                Data = data;
+            }
+        }
+
+        [Fact]
+        void TestRead()
+        {
+            const string hexBuffer = "A164446174616474657374";
+            Result<string> result = Helper.Read<Result<string>>(hexBuffer);
+
+            Assert.NotNull(result);
+            Assert.Equal("test", result.Data);
+        }
+
+        [Fact]
+        void TestWrite()
+        {
+            Result<string> result = new Result<string>("test");
+            string hexEncoded = null;
+            var ex = Record.Exception(() =>
+            {
+                hexEncoded = Helper.Write(result);
+            });
+            Assert.Null(ex);
+            Assert.NotNull(hexEncoded);
+            Assert.Equal("A164446174616474657374", hexEncoded);
+        }
+    }
+}
