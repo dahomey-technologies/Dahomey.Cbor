771e4347fd0fa5636c222b0b91d6c1072b4d1c85
Jørgen <jtorvald@users.noreply.github.com>
2019-09-19 23:22:39 +0200

    Tests for Issue 40 and Issue 41 (#42)

---
+++ src/Dahomey.Cbor.Tests/Issues/Issue0040.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using Dahomey.Cbor.Attributes;
+using Xunit;
+
+namespace Dahomey.Cbor.Tests.Issues
+{
+    public class Issue0040
+    {
+        public class WebserviceResponse
+        {
+            [CborProperty("status")]
+            public HttpStatusCode Status { get; set; } = HttpStatusCode.OK;
+        }
+
+        [Fact]
+        public void EnumConvert()
+        {
+            var data = "A16673746174757318C8";
+
+            var ex = Record.Exception(()=> {
+                Helper.Read<WebserviceResponse>(data);
+            });
+
+            Assert.Null(ex);
+        }
+    }
+}
