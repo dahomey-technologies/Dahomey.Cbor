27f4d80a80370e01c63fe5f614ce94d9d767954d
Michael Catanzariti <catanzariti@yahoo.com>
2019-12-31 23:41:27 +0100

    Added tests for interface collections and nullables

---
+++ src/Dahomey.Cbor.Tests/NullableTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace Dahomey.Cbor.Tests
+{
+    public class NullableTests
+    {
+        public class ObjectWithNullable
+        {
+            public int Id { get; set; }
+            public int? Nullable1 { get; set; }
+            public int? Nullable2 { get; set; }
+        }
+
+        [Fact]
+        public void TestRead()
+        {
+            CborOptions options = new CborOptions();
+
+            const string hexBuffer = "A36249640C694E756C6C61626C65310D694E756C6C61626C6532F6";
+            var obj = Helper.Read<ObjectWithNullable>(hexBuffer, options);
+
+            Assert.NotNull(obj);
+            Assert.Equal(12, obj.Id);
+            Assert.Equal(13, obj.Nullable1);
+            Assert.Null(obj.Nullable2);
+        }
+
+        [Fact]
+        public void TestWrite()
+        {
+            CborOptions options = new CborOptions();
+
+            const string hexBuffer = "A36249640C694E756C6C61626C65310D694E756C6C61626C6532F6";
+            var obj = new ObjectWithNullable
+            {
+                Id = 12,
+                Nullable1 = 13
+            };
+
+            Helper.TestWrite(obj, hexBuffer, null, options);
+        }
+    }
+}
