ff04c5360213972896ac230310b11dced51a53e3
Michael Catanzariti <catanzariti@yahoo.com>
2019-12-31 01:28:27 +0100

    Added ICborConverter.Read and ICborConverter.Write (based on object)

--- src/Dahomey.Cbor/Serialization/Converters/CborConverter.cs
+++ src/Dahomey.Cbor/Serialization/Converters/CborConverter.cs
@@ -2,18 +2,30 @@
 {
     public interface ICborConverter
     {
+        object Read(ref CborReader reader);
+        void Write(ref CborWriter writer, object value);
     }
 
     public interface ICborConverter<T> : ICborConverter
     {
-        T Read(ref CborReader reader);
+        new T Read(ref CborReader reader);
         void Write(ref CborWriter writer, T value);
         void Write(ref CborWriter writer, T value, LengthMode lengthMode);
     }
 
     public abstract class CborConverterBase<T> : ICborConverter<T>
     {
+        object ICborConverter.Read(ref CborReader reader)
+        {
+            return Read(ref reader);
+        }
+
         public abstract T Read(ref CborReader reader);
+
+        void ICborConverter.Write(ref CborWriter writer, object value)
+        {
+            Write(ref writer, (T)value);
+        }
 
         public virtual void Write(ref CborWriter writer, T value)
         {
